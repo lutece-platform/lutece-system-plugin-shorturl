2aef2c05883baa9e096e0ada3b33b4c168ff13c2
pierrelevy <pierrelevy@users.noreply.github.com>
2019-03-27 11:03:43 +0100

    SHORTURL-8 : Size of the target URL increased

--- src/sql/plugins/shortener/plugin/create_db_shorturl.sql
+++ src/sql/plugins/shortener/plugin/create_db_shorturl.sql
@@ -6,7 +6,7 @@
 DROP TABLE IF EXISTS shorturl;
 CREATE TABLE shorturl (		
 id_shorturl int default '0' NOT NULL,
-shorturl_url varchar(50) default '' NOT NULL,
+shorturl_url varchar(256) default '' NOT NULL,
 abbreviation varchar(50) default '' NOT NULL,
 creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 hits int default '0' NOT NULL,
